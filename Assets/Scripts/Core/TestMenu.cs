f3817b23dd1e276399e2915d8682dd8ae601c607
Auroiah Morgan <amwebdev@outlook.com>
2022-01-05 13:00:20 -0500

    Added quit button
    added temporary quit button for building and testing.

---
+++ Assets/Scripts/Core/TestMenu.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+namespace com.sluggagames.dragon
+{
+  public class TestMenu : MonoBehaviour
+  {
+
+    public void QuitGame()
+    {
+      Application.Quit();
+    }
+  }
+}
