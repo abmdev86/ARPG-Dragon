48dc01ce4d47be4f1071c70abed1458a58dd6f3d
Auroiah Morgan <amwebdev@outlook.com>
2022-01-06 23:57:16 -0500

    Adds Enemy Patrolling

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,6 +1,8 @@
 
+using System;
 using com.sluggagames.dragon.Combat;
 using com.sluggagames.dragon.Core;
+using com.sluggagames.dragon.Movement;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,32 +11,90 @@ namespace com.sluggagames.dragon.Control
   [RequireComponent(typeof(NavMeshAgent))]
   public class AIController : MonoBehaviour
   {
-    [Range(1,20)]
-    [SerializeField] float chaseDistance = 5f;
     GameObject player;
     Fighter fighter;
     Health health;
+    Mover mover;
+    Vector3 gaurdStartPosition;
+    float timeSinceLastSawPlayer = Mathf.Infinity;
+    int currentWaypointIndex = 0;
+    [SerializeField] float suspicionTime = 3;
+    [Range(1, 20)]
+    [SerializeField] float chaseDistance = 5f;
+    [SerializeField] PatrolPath patrolPath;
+    [SerializeField] float waypointTolerance = 1f;
+
 
     private void Start()
     {
       fighter = GetComponent<Fighter>();
       player = GameObject.FindGameObjectWithTag("Player");
       health = GetComponent<Health>();
+      gaurdStartPosition = transform.position;
+      mover = GetComponent<Mover>();
     }
     private void Update()
     {
       if (health.IsDead) return;
       if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
       {
-
-        fighter.Attack(player);
+        timeSinceLastSawPlayer = 0;
+        AttackBehaviour();
+      }
+      else if (timeSinceLastSawPlayer < suspicionTime)
+      {
+        //sus state
+        SusBehaviour();
       }
       else
       {
-        fighter.Cancel();
+        PatrolBehaviour();
       }
+      timeSinceLastSawPlayer += Time.deltaTime;
 
+    }
 
+    private void PatrolBehaviour()
+    {
+      GetComponent<NavMeshAgent>().speed = 2.75f;
+      Vector3 nextPosition = gaurdStartPosition;
+      if (patrolPath != null)
+      {
+        if (AtWaypoint())
+        {
+          CycleWaypoint();
+        }
+        nextPosition = GetCurrentWaypoint();
+
+      }
+      mover.StartMoveAction(nextPosition);
+    }
+
+    private Vector3 GetCurrentWaypoint()
+    {
+      return patrolPath.GetWaypointPosition(currentWaypointIndex);
+    }
+
+    private void CycleWaypoint()
+    {
+      currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+    }
+
+    private bool AtWaypoint()
+    {
+      float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+      return distanceToWaypoint < waypointTolerance;
+    }
+
+    private void SusBehaviour()
+    {
+      GetComponent<ActionScheduler>().CancelCurrentAction();
+      GetComponent<NavMeshAgent>().speed = 3.75f;
+    }
+
+    private void AttackBehaviour()
+    {
+      fighter.Attack(player);
     }
 
     private bool InAttackRangeOfPlayer()
@@ -42,7 +102,8 @@ namespace com.sluggagames.dragon.Control
       float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
       return distanceToPlayer < chaseDistance;
     }
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
       Gizmos.color = Color.blue;
       Gizmos.DrawWireSphere(transform.position, chaseDistance);
 
---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace com.sluggagames.dragon.Control
+{
+  public class PatrolPath : MonoBehaviour
+  {
+    [Range(0, 1)]
+    [SerializeField] float pointRadius = 1f;
+    private void OnDrawGizmos()
+    {
+      Gizmos.color = Color.red;
+      for (int i = 0; i < transform.childCount; i++)
+      {
+        int j = GetNextIndex(i);
+        Gizmos.DrawSphere(GetWaypointPosition(i), pointRadius);
+
+        Gizmos.DrawLine(GetWaypointPosition(i), GetWaypointPosition(j));
+      }
+    }
+
+    public int GetNextIndex(int i)
+    {
+      if (i + 1 == transform.childCount)
+      {
+        return 0;
+      }
+      return i + 1;
+    }
+
+    public Vector3 GetWaypointPosition(int i)
+    {
+      return transform.GetChild(i).position;
+    }
+  }
+}
