8ce4d16ae1e9c1a6e1b3b769edfc57022f6ac4c0
Auroiah Morgan <amwebdev@outlook.com>
2022-01-09 14:58:04 -0500

    Update CinematicControlRemover.cs
    Player can no longer move when cutscene triggers

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -2,27 +2,32 @@
 using UnityEngine;
 using UnityEngine.Playables;
 using com.sluggagames.dragon.Core;
+using com.sluggagames.dragon.Control;
 
 namespace com.sluggagames.dragon.Cinemamtics
 {
   public class CinematicControlRemover : MonoBehaviour
   {
+    GameObject player;
     PlayableDirector playableDirector;
     private void Start()
     {
+      player = GameObject.FindGameObjectWithTag("Player");
       playableDirector = GetComponent<PlayableDirector>();
       playableDirector.played += DisableControls;
       playableDirector.stopped += EnableControls;
     }
     void EnableControls(PlayableDirector pd)
     {
+      player.GetComponent<PlayerController>().enabled = true;
       print("Controls enabled ");
-      GameObject player = GameObject.FindGameObjectWithTag("Player");
-      player.GetComponent<ActionScheduler>().CancelCurrentAction();
+
     }
 
     void DisableControls(PlayableDirector pd)
     {
+      player.GetComponent<ActionScheduler>().CancelCurrentAction();
+      player.GetComponent<PlayerController>().enabled = false;
       print("Controls disabled ");
 
     }
