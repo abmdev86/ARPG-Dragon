591bf29d9ab350543e8e759a4f4a6b28a26dbda0
Auroiah Morgan <amwebdev@outlook.com>
2022-01-12 11:09:25 -0500

    Adds scene transition and portals

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,5 +1,8 @@
 
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 
@@ -8,12 +11,43 @@ namespace com.sluggagames.dragon.SceneManagement
   public class Portal : MonoBehaviour
   {
     [SerializeField] int sceneToLoad = -1;
+    [SerializeField] Transform spawnPoint;
     private void OnTriggerEnter(Collider other)
     {
       if (other.tag == "Player")
       {
-        SceneManager.LoadScene(sceneToLoad);
+        StartCoroutine(Transition());
       }
+    }
+
+    IEnumerator Transition()
+    {
+      DontDestroyOnLoad(gameObject);
+      yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+      Portal otherPortal = GetOtherPortal();
+      UpdatePlayer(otherPortal);
+      print(otherPortal.spawnPoint.position);
+      Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+      GameObject player = GameObject.FindGameObjectWithTag("Player");
+      player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+      player.transform.position = otherPortal.spawnPoint.position;
+      player.transform.rotation = otherPortal.spawnPoint.rotation;
+
+    }
+
+    private Portal GetOtherPortal()
+    {
+      foreach (Portal portal in FindObjectsOfType<Portal>())
+      {
+        if (portal == this) continue;
+        return portal;
+      }
+      return null; // no portal found!
     }
   }
 }
